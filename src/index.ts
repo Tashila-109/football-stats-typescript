e67c3a867663d31497a905dc4dd193144fbca324
Tashila <tashila.dev@gmail.com>
2022-10-16 23:42:49 +0400

    Updated index ts file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
-let manUnitedWins = 0;
-
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
